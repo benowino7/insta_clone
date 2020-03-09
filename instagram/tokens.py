14d522b741a50d793c5236a504bf1b65da968b45
benowino7 <bensonowino7@gmail.com>
2020-03-09 11:14:45 +0300

    added some comments to the tokens.py file

--- instagram/tokens.py
+++ instagram/tokens.py
@@ -1,8 +1,10 @@
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils import six
+from django.contrib.auth.tokens import PasswordResetTokenGenerator #generates passwords for users in encrypted form
+from django.utils import six #It is intended to support codebases 2,3 without modifications
 
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
   def _make_hash_value(self, user, timestamp):
     return (six.text_type(user.pk) + six.text_type(timestamp) + six.text_type(user.profile.email_confirmed))
 
 account_activation_token = AccountActivationTokenGenerator()
+
+#make up a DatetimeIndex(timestamp)
